e51bd3cf6244cde761eb0afef8a4cdeb15ef94df
Jeet Zala <jeetzala074@gmail.com>
2026-03-22 10:31:04 +0530

    fix server config

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,10 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: "0.0.0.0",
+  port: env.int("PORT", 1337),
+  url: "https://ai-recipe-platform.onrender.com",
+  proxy: true,
   app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    keys: env.array("APP_KEYS"),
   },
 });
